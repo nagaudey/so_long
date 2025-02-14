c5cda55be35c3297ece958f9b225be3f8627ecd9
nagaudey <ngaudey@gmail.com>
2025-02-14 02:27:10 +0100

    lib

---
+++ obj/ft_cutmap.d
@@ -0,0 +1,7 @@
+obj/ft_cutmap.o: ft_cutmap.c so_long.h libft/all.h minilibx-linux/mlx.h
+
+so_long.h:
+
+libft/all.h:
+
+minilibx-linux/mlx.h:
---
+++ obj/main.d
@@ -0,0 +1,7 @@
+obj/main.o: main.c so_long.h libft/all.h minilibx-linux/mlx.h
+
+so_long.h:
+
+libft/all.h:
+
+minilibx-linux/mlx.h:
